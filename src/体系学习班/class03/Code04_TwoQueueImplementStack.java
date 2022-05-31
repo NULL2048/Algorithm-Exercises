30e4374b5b33cd991713ceaa3dd925533061e691
NULL2048 <973071263@qq.com>
2022-05-31 20:26:06 +0800

    如何用队列结构实现栈结构

---
+++ src/体系学习班/class03/Code04_TwoQueueImplementStack.java
@@ -0,0 +1,51 @@
+package 体系学习班.class03;
+
+/**
+ * 如何用队列结构实现栈结构
+ */
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Code04_TwoQueueImplementStack {
+    public static class TwoQueueStack<T> {
+        public Queue<T> queue;
+        public Queue<T> help;
+
+        public TwoQueueStack() {
+            queue = new LinkedList<>();
+            help = new LinkedList<>();
+        }
+
+        public void push(T value) {
+            queue.offer(value);
+        }
+
+        public T poll() {
+            while (queue.size() > 1) {
+                help.offer(queue.poll());
+            }
+            T ans = queue.poll();
+            Queue<T> tmp = queue;
+            queue = help;
+            help = tmp;
+            return ans;
+        }
+
+        public T peek() {
+            while (queue.size() > 1) {
+                help.offer(queue.poll());
+            }
+            T ans = queue.poll();
+            help.offer(ans);
+            Queue<T> tmp = queue;
+            queue = help;
+            help = tmp;
+            return ans;
+        }
+
+        public boolean isEmpty() {
+            return queue.isEmpty();
+        }
+
+    }
+}
