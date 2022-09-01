7165914ea5bee2354a1243dd56eeeb6f2867990b
NULL2048 <973071263@qq.com>
2022-09-01 22:15:44 +0800

    迈台阶问题 奶牛生小牛问题 （斐波那契数列问题推广）

--- src/体系学习班/class26/Code02_FibonacciProblem.java
+++ src/体系学习班/class26/Code02_FibonacciProblem.java
@@ -13,7 +13,7 @@ public class Code02_FibonacciProblem {
         return f1(n - 1) + f1(n - 2);
     }
 
-    // 2、也使用过递推去求斐波那契数列，但是这个是直接用迭代，没用递归
+    // 2、也使用过递推去求斐波那契数列，但是这个是直接用迭代，没用递归，线性求解方法
     public static int f2(int n) {
         if (n < 1) {
             return 0;
@@ -32,7 +32,7 @@ public class Code02_FibonacciProblem {
         return res;
     }
 
-    // 3、使用优化的方法去求指定位置的斐波那契数列，时间复杂度O(logN)
+    // 3、使用优化的方法去求指定位置的斐波那契数列，时间复杂度O(logN)。（矩阵+快速幂）
     public static int f3(int n) {
         if (n < 1) {
             return 0;
@@ -44,8 +44,8 @@ public class Code02_FibonacciProblem {
         // [ 1 ,1 ]
         // [ 1, 0 ]
         int[][] base = {
-                { 1, 1 },
-                { 1, 0 }
+                {1, 1},
+                {1, 0}
         };
         // 计算等号左边的两个位置的数组成的矩阵
         int[][] res = matrixPower(base, n - 2);
@@ -85,9 +85,9 @@ public class Code02_FibonacciProblem {
         int k = a[0].length; // a的列数同时也是b的行数
         int[][] ans = new int[n][m];
         // 矩阵相乘的步骤
-        for(int i = 0 ; i < n; i++) {
-            for(int j = 0 ; j < m;j++) {
-                for(int c = 0; c < k; c++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int c = 0; c < k; c++) {
                     ans[i][j] += a[i][c] * b[c][j];
                 }
             }
@@ -97,6 +97,7 @@ public class Code02_FibonacciProblem {
 
 
     // 后面是别的例子，只看上面斐波那契数列的即可
+    // 下面这道题是跳台阶问题的解题方法
     public static int s1(int n) {
         if (n < 1) {
             return 0;
@@ -132,11 +133,13 @@ public class Code02_FibonacciProblem {
         if (n == 1 || n == 2) {
             return n;
         }
-        int[][] base = { { 1, 1 }, { 1, 0 } };
+        int[][] base = {{1, 1}, {1, 0}};
         int[][] res = matrixPower(base, n - 2);
         return 2 * res[0][0] + res[1][0];
     }
 
+    // 下面这个就是奶牛生小牛问题的解题方法
+    // 1、利用递推关系，通过递归求
     public static int c1(int n) {
         if (n < 1) {
             return 0;
@@ -147,6 +150,7 @@ public class Code02_FibonacciProblem {
         return c1(n - 1) + c1(n - 3);
     }
 
+    // 2、利用递推关系，通过迭代求
     public static int c2(int n) {
         if (n < 1) {
             return 0;
@@ -169,6 +173,21 @@ public class Code02_FibonacciProblem {
         return res;
     }
 
+    // 3、利用优化后的方法求
+    /*
+        思路：先进行推理
+        第一年：A(成熟)-->1个
+        第二年：A(成熟)，B-->2个
+        第三年：A(成熟)，B，C-->3个
+        第四年：A(成熟)，B(成熟)，C，D-->4个
+        第五年：A(成熟)，B(成熟)，C(成熟)，D，E，F-->6个
+        第六年：A(成熟)，B(成熟)，C(成熟)，D(成熟)，E，F，G，H，I-->9个
+        经过推演，可以发现第N年的牛的个数F(N)=F(N-1)+0*F(N-2)+F(N-3)
+        就是去年出生的今年还会活着，而三年前就已经出生的，现在肯定是成熟的，可以生新的小牛
+        |F(N),F(N-1),F(N-2)| = |F(3),F(2),F(1)|*|3*3|^(N-3)
+        这是一个三阶问题
+        我发现在计算base矩阵的时候，那些个矩阵中的值不是0就是1，有时候可以合理猜测节省解方程时间
+    */
     public static int c3(int n) {
         if (n < 1) {
             return 0;
@@ -176,11 +195,13 @@ public class Code02_FibonacciProblem {
         if (n == 1 || n == 2 || n == 3) {
             return n;
         }
+        // 这个就是这个递推式求出来的矩阵，这个矩阵是需要我们自己手算的，列几个等式解方程
         int[][] base = {
-                { 1, 1, 0 },
-                { 0, 0, 1 },
-                { 1, 0, 0 } };
+                {1, 1, 0},
+                {0, 0, 1},
+                {1, 0, 0}};
         int[][] res = matrixPower(base, n - 3);
+        // 根据等式来求出第n项的值
         return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
     }
 
