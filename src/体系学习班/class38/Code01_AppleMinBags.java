c89a9a14d7cefec024aca6fb536be41d1fc97929
NULL2048 <973071263@qq.com>
2022-09-21 14:52:02 +0800

    小虎买苹果 小虎去买苹果，商店只提供两种类型的塑料袋，每种类型都有任意数量。 1）能装下6个苹果的袋子（6号袋子） 2）能装下8个苹果的袋子（8号袋子） 小虎可以自由使用两种袋子来装苹果，但是小虎有强迫症，他要求自己使用的袋子数量必须最少，且使用的每个袋子必须装满。 给定一个正整数N，返回至少使用多少袋子。如果N无法让使用的每个袋子必须装满，返回-1
    【打表找规律（对数器找规律）】

---
+++ src/体系学习班/class38/Code01_AppleMinBags.java
@@ -0,0 +1,42 @@
+package 体系学习班.class38;
+
+public class Code01_AppleMinBags {
+    // 暴力解，用暴力解先找一下规律
+    public static int minBags(int apple) {
+        if (apple < 0) {
+            return -1;
+        }
+        int bag8 = (apple >> 3);
+        int rest = apple - (bag8 << 3);
+        while(bag8 >= 0) {
+            // rest 个
+            if(rest % 6 ==0) {
+                return bag8 + (rest / 6);
+            } else {
+                bag8--;
+                rest += 8;
+            }
+        }
+        return -1;
+    }
+
+    // 根据找到的规律，再写一个O(1)的代码
+    public static int minBagAwesome(int apple) {
+        if ((apple & 1) != 0) { // 如果是奇数，返回-1
+            return -1;
+        }
+        // hardcode
+        if (apple < 18) {
+            return apple == 0 ? 0 : (apple == 6 || apple == 8) ? 1
+                    : (apple == 12 || apple == 14 || apple == 16) ? 2 : -1;
+        }
+        return (apple - 18) / 8 + 3;
+    }
+
+    public static void main(String[] args) {
+        for(int apple = 1; apple < 200;apple++) {
+            System.out.println(apple + " : "+ minBags(apple));
+        }
+
+    }
+}
