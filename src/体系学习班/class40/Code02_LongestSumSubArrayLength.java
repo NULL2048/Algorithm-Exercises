9e9ed27b8ff6140e2e30cec122bb2715bd883587
NULL2048 <973071263@qq.com>
2022-09-25 14:24:32 +0800

    整数数组子数组累加和等于K的最大长度 给定一个整数组成的无序数组arr，值可能正、可能负、可能0 给定一个整数值K，找到arr的所有子数组里，哪个子数组的累加和等于K，并且是长度最大的 返回其长度。
    【子数组达到规定累加和的最大长度系列问题】【预处理数组】

---
+++ src/体系学习班/class40/Code02_LongestSumSubArrayLength.java
@@ -0,0 +1,95 @@
+package 体系学习班.class40;
+
+import java.util.HashMap;
+
+public class Code02_LongestSumSubArrayLength {
+
+    public static int maxLength(int[] arr, int k) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        // key：前缀和
+        // value : 0~value这个前缀和是最早出现key这个值的
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // 提前添加前缀和为0，在-1位置的记录，否则将错过所有0开头的答案
+        map.put(0, -1); // important
+        int len = 0;
+        int sum = 0;
+        // 构造预处理结构前缀和map    只记录最早出现的位置
+        // 一边构造，也一边执行流程
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            if (map.containsKey(sum - k)) {
+                len = Math.max(i - map.get(sum - k), len);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return len;
+    }
+
+    // for test
+    public static int right(int[] arr, int K) {
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (valid(arr, i, j, K)) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        return max;
+    }
+
+    // for test
+    public static boolean valid(int[] arr, int L, int R, int K) {
+        int sum = 0;
+        for (int i = L; i <= R; i++) {
+            sum += arr[i];
+        }
+        return sum == K;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int size, int value) {
+        int[] ans = new int[(int) (Math.random() * size) + 1];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = (int) (Math.random() * value) - (int) (Math.random() * value);
+        }
+        return ans;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        for (int i = 0; i != arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int len = 50;
+        int value = 100;
+        int testTime = 500000;
+
+        System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(len, value);
+            int K = (int) (Math.random() * value) - (int) (Math.random() * value);
+            int ans1 = maxLength(arr, K);
+            int ans2 = right(arr, K);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println("K : " + K);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                break;
+            }
+        }
+        System.out.println("test end");
+
+    }
+
+}
