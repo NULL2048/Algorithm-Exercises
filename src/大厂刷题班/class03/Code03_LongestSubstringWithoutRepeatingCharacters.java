e7f94451cb8af00029097ea997561ebfad68e184
NULL2048 <973071263@qq.com>
2022-10-04 18:45:18 +0800

    无重复字符的最长子串 求一个字符串中，最长无重复字符子串长度 【动态规划】【空间压缩】
    测试链接：3. 无重复字符的最长子串 - 力扣（LeetCode）

---
+++ src/大厂刷题班/class03/Code03_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,138 @@
+package 大厂刷题班.class03;
+
+// 动态规划 空间压缩
+// 本题测试链接 : https://leetcode.com/problems/longest-substring-without-repeating-characters/
+public class Code03_LongestSubstringWithoutRepeatingCharacters {
+    // 1、暴力递归
+    public int lengthOfLongestSubstring1(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] str = s.toCharArray();
+        int[] preMap = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = -1;
+        }
+
+        int[] temp = new int[256];
+        for (int i = 0; i < 256; i++) {
+            temp[i] = -1;
+        }
+        // 构造每一个位置的前面出现的位置表，预处理结构
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = temp[str[i]];
+            temp[str[i]] = i;
+        }
+
+        int maxLen = Integer.MIN_VALUE;
+        // 尝试以每一个位置为右边界起点，来尝试找到最长的无重复字符串
+        for (int i = 0; i < str.length; i++) {
+            maxLen = Math.max(maxLen, process1(i, str, preMap));
+        }
+        return maxLen;
+    }
+
+    // 从index向左开始，最长的没有重复字符的字符串长度    从右向左尝试
+    public int process1(int index, char[] str, int[] preMap) {
+        // basecase  当index==0时，长度肯定是1
+        if (index == 0) {
+            return 1;
+        }
+
+        // 因素一：当前字符上次出现的位置
+        int p1 = preMap[index] + 1;
+        // 因素二：当前index位置，前一个index-1位置往左推的距离
+        int p2 = index - 1 - process1(index - 1, str, preMap) + 1;
+
+        // 找到这两个因素中与index形成的长度最短的，并且在后面加1（加上index），作为最长的没有重复的字符串长度返回
+        return index - Math.max(p1, p2) + 1;
+
+    }
+
+
+    // 2、记忆化搜索
+    public int lengthOfLongestSubstring2(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] str = s.toCharArray();
+        int[] preMap = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = -1;
+        }
+
+        int[] temp = new int[256];
+        for (int i = 0; i < 256; i++) {
+            temp[i] = -1;
+        }
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = temp[str[i]];
+            temp[str[i]] = i;
+        }
+
+        // 不用给dp先初始化为-1，直接用0来表示还没有计算过即可，因为每一个位置的答案如果计算过的话最少也是长度1，不可能出现长度为0的情况，所以这里就默认用0表示还没有计算过即可。
+        int[] dp = new int[str.length];
+        int maxLen = Integer.MIN_VALUE;
+        for (int i = 0; i < str.length; i++) {
+            maxLen = Math.max(maxLen, process2(i, str, preMap, dp));
+        }
+        return maxLen;
+    }
+
+    public int process2(int index, char[] str, int[] preMap, int[] dp) {
+        if (dp[index] != 0) {
+            return dp[index];
+        }
+
+        if (index == 0) {
+            dp[0] = 1;
+            return 1;
+        }
+
+        int p1 = preMap[index] + 1;
+        int p2 = index - 1 - process2(index - 1, str, preMap, dp) + 1;
+
+        dp[index] = index - Math.max(p1, p2) + 1;
+        return dp[index];
+
+    }
+
+    // 3、动态规划
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+
+        char[] str = s.toCharArray();
+        int[] preMap = new int[256];
+        // 用来记录从左向右遍历，每一个字符的最近出现的位置
+        for (int i = 0; i < 256; i++) {
+            preMap[i] = -1;
+        }
+
+        // 不用给dp先初始化为-1，直接用0来表示还没有计算过即可，因为每一个位置的答案如果计算过的话最少也是长度1，不可能出现长度为0的情况，所以这里就默认用0表示还没有计算过即可。
+        //int[] dp = new int[str.length];
+        int maxLen = 1;
+        // 空间压缩，记录i-1位置能去到的最长无重复字符串长度
+        int preLen = 1;
+        // 将0下标的字符提前写入，最近一次出现位置为0
+        preMap[str[0]] = 0;
+
+        // 从下表1开始向右遍历
+        for (int i = 1; i < str.length; i++) {
+            // 因素一
+            int p1 = preMap[str[i]] + 1;
+            // 因素二
+            int p2 = i - preLen;
+            // 将当前i的最长长度写入到preLen，用于下一轮循环
+            preLen = i - Math.max(p1, p2) + 1;
+            // 找到最大长度
+            maxLen = Math.max(maxLen, preLen);
+            // 更新i位置字符的最新出现的位置
+            preMap[str[i]] = i;
+        }
+        return maxLen;
+    }
+}
--- src/测试/Solution.java
+++ src/测试/Solution.java
@@ -2,82 +2,49 @@ package 测试;
 
 import java.util.HashMap;
 class Solution {
-    // public int findTargetSumWays(int[] nums, int target) {
-    //     return process(nums, 0, target);
-    // }
-
-    // public int process(int[] nums, int index, int rest) {
-    //     if (index == nums.length) {
-    //         return rest == 0 ? 1 : 0;
-    //     }
-
-    //     return process(nums, index + 1, rest + nums[index]) + process(nums, index + 1, rest - nums[index]);
-    // }
-
-    // public int findTargetSumWays(int[] nums, int target) {
-    //     // 因为可变参数有可能为负数，我们这里就不用二维数组了，直接用map
-    //     HashMap<Integer, HashMap<Integer, Integer>> dp = new HashMap<>();
-    //     return process(nums, 0, target, dp);
-    // }
-
-    // public int process(int[] nums, int index, int rest, HashMap<Integer, HashMap<Integer, Integer>> dp) {
-    //     if (dp.containsKey(index) && dp.get(index).containsKey(rest)) {
-    //         return dp.get(index).get(rest);
-    //     }
-
-    //     if (index == nums.length) {
-    //         return rest == 0 ? 1 : 0;
-    //     }
-
-    //     int ans = process(nums, index + 1, rest + nums[index], dp) + process(nums, index + 1, rest - nums[index], dp);
-    //     if (!dp.containsKey(index)) {
-    //         dp.put(index, new HashMap<>());
-    //     }
-    //     dp.get(index).put(rest, ans);
-    //     return ans;
-    // }
-
-
-
-
-
-    public static int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        int sum = 0;
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = nums[i];
-            sum += arr[i];
-        }
-
-        if (Math.abs(target) > sum || ((target & 1) ^ (sum & 1)) != 0) {
+    public static int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
             return 0;
         }
 
-        int[][] dp = new int[n + 1][(sum << 1) + 2];
-
-        dp[n][0] = 1;
-
-        for (int index = n - 1; index >= 0; index--) {
-            for (int rest = -sum; rest <= sum; rest++) {
-                int ans = 0;
-                if (rest + arr[index] <= sum && rest + arr[index] >= -sum) {
-                    ans = dp[index + 1][(rest + arr[index]) <= 0 ? -(rest + arr[index]) : (((rest + arr[index]) << 1) + 1)];
-                }
-
-                if (rest - arr[index] <= sum  && rest - arr[index] >= -sum) {
-                    ans += dp[index + 1][(rest - arr[index]) <= 0 ? -(rest - arr[index]) : (((rest - arr[index]) << 1) + 1)];
-                }
-                dp[index][(rest <= 0 ? -rest : ((rest << 1)) + 1)] = ans;
-            }
+        char[] str = s.toCharArray();
+        int[] preMap = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = -1;
         }
 
-        return dp[0][(target <= 0 ? -target : ((target << 1) + 1))];
+        int[] temp = new int[256];
+        for (int i = 0; i < 256; i++) {
+            temp[i] = -1;
+        }
+        for (int i = 0; i < str.length; i++) {
+            preMap[i] = temp[str[i]];
+            temp[str[i]] = i;
+        }
+
+        int maxLen = Integer.MIN_VALUE;
+        for (int i = 0; i < str.length; i++) {
+            maxLen = Math.max(maxLen, process(str.length - 1, str, preMap));
+        }
+        return maxLen;
+    }
+
+    public static int process(int index, char[] str, int[] preMap) {
+        if (index == 0) {
+            return 1;
+        }
+
+        int p1 = preMap[index] + 1;
+        int p2 = index - 1 - process(index - 1, str, preMap) + 1;
+
+        return index - Math.max(p1, p2) + 1;
+
     }
 
     public static void main(String[] args) {
         int[] nums = {1,2,1};
-        System.out.println(findTargetSumWays(nums, 0));
+        String str = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
     }
 
 
