b03c6542e0acb845e07f044ff26ea40a7f19f40c
NULL2048 <973071263@qq.com>
2022-10-14 14:04:16 +0800

    编辑距离问题 给定两个字符串str1和str2，再给定三个整数ic、dc和rc，分别代表插入、删除和替换一个字符的代价，返回将str1编辑成str2的最小代价。
    【举例】
    	• str1="abc"，str2="adc"，ic=5，dc=3，rc=2 从"abc"编辑成"adc"，把'b'替换成'd'是代价最小的，所以返回2
    	• str1="abc"，str2="adc"，ic=5，dc=3，rc=100 从"abc"编辑成"adc"，先删除'b'，然后插入'd'是代价最小的，所以返回8
    	• str1="abc"，str2="abc"，ic=5，dc=3，rc=2 不用编辑了，本来就是一样的字符串，所以返回0
    【经典中的经典】【一个样本做行个样本做列】【样本对应模型】【空间压缩】【设计简化外部信息的动态规划】
    
    测试链接（阉割版）：72. 编辑距离 - 力扣（LeetCode）

---
+++ src/大厂刷题班/class05/Code03_EditCost.java
@@ -0,0 +1,111 @@
+package 大厂刷题班.class05;
+
+// 样本对应模型  空间压缩
+// 阉割版  https://leetcode.cn/problems/edit-distance/   这个就是把删除，替换，添加操作的代价都固定成了1
+public class Code03_EditCost {
+    public int minDistance(String word1, String word2) {
+        if (word1 == null || word2 == null) {
+            return 0;
+        }
+
+        char[] s1 = word1.toCharArray();
+        char[] s2 = word2.toCharArray();
+        return minCost(s1, s2, 1, 1, 1);
+    }
+
+    // for test
+    public static int minCost(char[] s1, char[] s2, int ac, int rc, int dc) {
+        // dp[i][j]：s1前缀取前i个字符，编辑成s2前缀取j个字符，将s1的前i个字符组成的前缀串如何用最少编辑代价转换成s2的前j个字符的前缀串，最少代价是多少。
+        // dp[i][j] = -1表示当前状态下无法编辑成功
+        int[][] dp = new int[s1.length + 1][s2.length + 1];
+
+        // 初始化dp数组  规定dp[0][0] = 0
+        // 0列s2编辑成s1空串，只能删除
+        for (int i = 1; i <= s1.length; i++) {
+            dp[i][0] = dc * i;
+        }
+        // 0行s1空串编辑为s2，只能添加
+        for (int j = 1; j <= s2.length; j++) {
+            dp[0][j] = ac * j;
+        }
+
+        for (int i = 1; i <= s1.length; i++) {
+            for (int j = 1; j <= s2.length; j++) {
+                // 分情况讨论
+                // 可能性1：s1的前i-1个字符可以变成s2的前j个字符，即删掉s1的最后一个i位置的字符即可完成编辑，如果dp[i - 1][j] == -1，说明无法编辑成功，赋值为-1
+                int p1 = dp[i - 1][j] != -1 ? dp[i - 1][j] + dc : -1;
+                // 可能性2：s1的前i个字符先变成str2的前j-1个字符，后再加上s2的最后一个j位置的字符即可完成编辑。如果dp[i][j - 1] == -1，说明无法编辑成功，赋值为-1
+                int p2 = dp[i][j - 1] != -1 ? dp[i][j - 1] + ac : -1;
+                // 可能性3: s1, s2两个字符串最后一个字符串相等，如果str1前面i-1个字符可以编辑成s2前面j-1个字符，那么最后一个字符保留即可完成编辑。
+                // 可能性4：s1, s2两个字符串最后一个字符串不相等，如果str1前面i-1个字符可以编辑成s2前面j-1个字符，那么最后一个字符替换即可完成编辑。
+                int p3 = s1[i - 1] == s2[j - 1] ? (dp[i - 1][j - 1] != -1 ? dp[i - 1][j - 1] : -1) : (dp[i - 1][j - 1] != -1 ? dp[i - 1][j - 1] + rc : -1);
+
+                // 取四种情况的最小值
+                dp[i][j] = Math.min(p1, Math.min(p2, p3));
+            }
+        }
+        // 返回答案
+        return dp[s1.length][s2.length];
+    }
+
+
+    // for test
+    public static int minCost2(String str1, String str2, int ic, int dc, int rc) {
+        if (str1 == null || str2 == null) {
+            return 0;
+        }
+        char[] chs1 = str1.toCharArray();
+        char[] chs2 = str2.toCharArray();
+        char[] longs = chs1.length >= chs2.length ? chs1 : chs2;
+        char[] shorts = chs1.length < chs2.length ? chs1 : chs2;
+        if (chs1.length < chs2.length) {
+            int tmp = ic;
+            ic = dc;
+            dc = tmp;
+        }
+        int[] dp = new int[shorts.length + 1];
+        for (int i = 1; i <= shorts.length; i++) {
+            dp[i] = ic * i;
+        }
+        for (int i = 1; i <= longs.length; i++) {
+            int pre = dp[0];
+            dp[0] = dc * i;
+            for (int j = 1; j <= shorts.length; j++) {
+                int tmp = dp[j];
+                if (longs[i - 1] == shorts[j - 1]) {
+                    dp[j] = pre;
+                } else {
+                    dp[j] = pre + rc;
+                }
+                dp[j] = Math.min(dp[j], dp[j - 1] + ic);
+                dp[j] = Math.min(dp[j], tmp + dc);
+                pre = tmp;
+            }
+        }
+        return dp[shorts.length];
+    }
+
+    public static void main(String[] args) {
+        String str1 = "ab12cd3";
+        String str2 = "abcdf";
+        System.out.println(minCost(str1.toCharArray(), str2.toCharArray(), 5, 3, 2));
+        System.out.println(minCost2(str1, str2, 5, 3, 2));
+
+        str1 = "abcdf";
+        str2 = "ab12cd3";
+        System.out.println(minCost(str1.toCharArray(), str2.toCharArray(), 3, 2, 4));
+        System.out.println(minCost2(str1, str2, 3, 2, 4));
+
+        str1 = "";
+        str2 = "ab12cd3";
+        System.out.println(minCost(str1.toCharArray(), str2.toCharArray(), 1, 7, 5));
+        System.out.println(minCost2(str1, str2, 1, 7, 5));
+
+        str1 = "abcdf";
+        str2 = "";
+        System.out.println(minCost(str1.toCharArray(), str2.toCharArray(), 2, 9, 8));
+        System.out.println(minCost2(str1, str2, 2, 9, 8));
+
+    }
+
+}
