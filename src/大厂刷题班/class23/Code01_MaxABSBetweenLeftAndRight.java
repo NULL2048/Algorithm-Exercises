71de993f7e0af7e8b2f56cc4f44e9d3a41a6d9d1
NULL2048 <973071263@qq.com>
2022-11-29 16:00:25 +0800

    找出最大的左部分最大值减去右部分最大值的绝对值 给定一个数组arr长度为N(N>1)，你可以把任意长度大于0且小于N的前缀作为左部分，剩下的 作为右部分。 但是每种划分下都有左部分的最大值和右部分的最大值，请返回最大的， 左部分最大值减去右部分最大值的绝对值。 【数组】【辅助数组】

---
+++ src/大厂刷题班/class23/Code01_MaxABSBetweenLeftAndRight.java
@@ -0,0 +1,69 @@
+package 大厂刷题班.class23;
+
+// 找出最大的左部分最大值减去右部分最大值的绝对值
+// 数组   辅助数组
+public class Code01_MaxABSBetweenLeftAndRight {
+    // 这道题的算法流程都很简单，代码流程也比较清晰明了，就不写注释了，一看就懂
+
+    // 1、暴力解
+    public static int maxABS1(int[] arr) {
+        int res = Integer.MIN_VALUE;
+        int maxLeft = 0;
+        int maxRight = 0;
+        for (int i = 0; i != arr.length - 1; i++) {
+            maxLeft = Integer.MIN_VALUE;
+            for (int j = 0; j != i + 1; j++) {
+                maxLeft = Math.max(arr[j], maxLeft);
+            }
+            maxRight = Integer.MIN_VALUE;
+            for (int j = i + 1; j != arr.length; j++) {
+                maxRight = Math.max(arr[j], maxRight);
+            }
+            res = Math.max(Math.abs(maxLeft - maxRight), res);
+        }
+        return res;
+    }
+
+    // 2、利用辅助数组
+    public static int maxABS2(int[] arr) {
+        int[] lArr = new int[arr.length];
+        int[] rArr = new int[arr.length];
+        lArr[0] = arr[0];
+        rArr[arr.length - 1] = arr[arr.length - 1];
+        for (int i = 1; i < arr.length; i++) {
+            lArr[i] = Math.max(lArr[i - 1], arr[i]);
+        }
+        for (int i = arr.length - 2; i > -1; i--) {
+            rArr[i] = Math.max(rArr[i + 1], arr[i]);
+        }
+        int max = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            max = Math.max(max, Math.abs(lArr[i] - rArr[i + 1]));
+        }
+        return max;
+    }
+
+    // 3、最优解
+    public static int maxABS3(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+        }
+        return max - Math.min(arr[0], arr[arr.length - 1]);
+    }
+
+    public static int[] generateRandomArray(int length) {
+        int[] arr = new int[length];
+        for (int i = 0; i != arr.length; i++) {
+            arr[i] = (int) (Math.random() * 1000) - 499;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray(200);
+        System.out.println(maxABS1(arr));
+        System.out.println(maxABS2(arr));
+        System.out.println(maxABS3(arr));
+    }
+}
