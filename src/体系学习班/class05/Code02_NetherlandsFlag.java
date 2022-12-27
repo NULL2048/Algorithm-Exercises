e18f4ffa8b7483a78d8b8fa5dbce84b94d14c9fe
NULL2048 <973071263@qq.com>
2022-12-27 15:13:47 +0800

    为荷兰国旗问题补充一个力扣的测试链接
    颜色分类
    给定一个包含红色、白色和蓝色、共 n 个元素的数组 nums ，原地对它们进行排序，使得相同颜色的元素相邻，并按照红色、白色、蓝色顺序排列。
    我们使用整数 0、 1 和 2 分别表示红色、白色和蓝色。
    必须在不使用库内置的 sort 函数的情况下解决这个问题。
    【荷兰国旗问题】【快排】
    
    测试链接：75. 颜色分类 - 力扣（LeetCode）

--- src/体系学习班/class05/Code02_NetherlandsFlag.java
+++ src/体系学习班/class05/Code02_NetherlandsFlag.java
@@ -1,10 +1,8 @@
 package 体系学习班.class05;
 
-import static 排序.堆排序.swap;
-
 // 荷兰国旗问题
 public class Code02_NetherlandsFlag {
-    public static int[] partition(int[] arr, int l, int r, int num) {
+    public int[] partition(int[] arr, int l, int r, int num) {
         int less = l - 1;
         int more = r + 1;
         int cur = l;
@@ -20,4 +18,33 @@ public class Code02_NetherlandsFlag {
         return new int[] { less + 1, more - 1 };
     }
 
+    // https://leetcode.cn/problems/sort-colors/
+    // 力扣的这道题本质就是荷兰国旗问题，用来划分的值就是0、1、2，下面的代码可以直接提交
+    public void sortColors(int[] nums) {
+        // 值为0的右区间边界
+        int l = -1;
+        // 值为2的左区间边界
+        int r = nums.length;
+        // 当前遍历到的数
+        int cur = 0;
+
+        while (cur < r) {
+            if (nums[cur] < 1) {
+                // 这里要注意cur++，因为交换到cur位置的数原本就在l~cur之前，这个范围cur已经滑过了，那么这个区间内的数一定都是等于1的，所以交换到cur位置他就一定是在中间区间的数，后面就直接让cur++去判断后面还没有滑过的数即可
+                swap(nums, ++l, cur++);
+            } else if (nums[cur] > 1) {
+                // 因为交换到cur位置的数一定是原本在cur~r之间的数，这个范围cur还没有滑过，所以不能确定这个数和1的大小关系，所以这里cur不变，还是要在后面的循环中继续判断交换过来的数与1的大小关系
+                swap(nums, --r, cur);
+            } else {
+                cur++;
+            }
+        }
+    }
+
+    // 交换两个数的位置
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
