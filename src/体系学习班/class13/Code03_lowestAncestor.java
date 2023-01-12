a58a64e597f067acb67da92266e3c42fe1a51659
NULL2048 <973071263@qq.com>
2023-01-12 14:52:37 +0800

    第八题：给定一棵二叉树的头节点head，和另外两个节点a和b，返回a和b的最低公共祖先 测试链接：236. 二叉树的最近公共祖先 - 力扣（LeetCode） 进阶题目：二叉树节点间最近公共祖先的批量查询问题【高频题】 【二叉树递归套路】

--- src/体系学习班/class13/Code03_lowestAncestor.java
+++ src/体系学习班/class13/Code03_lowestAncestor.java
@@ -3,28 +3,28 @@ package 体系学习班.class13;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-
+// https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
 public class Code03_lowestAncestor {
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
+    public static class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
 
-        public Node(int data) {
-            this.value = data;
+        public TreeNode(int data) {
+            this.val = data;
         }
     }
 
-    public static Node lowestAncestor1(Node head, Node o1, Node o2) {
+    public static TreeNode lowestAncestor1(TreeNode head, TreeNode o1, TreeNode o2) {
         if (head == null) {
             return null;
         }
         // key的父节点是value
-        HashMap<Node, Node> parentMap = new HashMap<>();
+        HashMap<TreeNode, TreeNode> parentMap = new HashMap<>();
         parentMap.put(head, null);
         fillParentMap(head, parentMap);
-        HashSet<Node> o1Set = new HashSet<>();
-        Node cur = o1;
+        HashSet<TreeNode> o1Set = new HashSet<>();
+        TreeNode cur = o1;
         o1Set.add(cur);
         while (parentMap.get(cur) != null) {
             cur = parentMap.get(cur);
@@ -37,7 +37,7 @@ public class Code03_lowestAncestor {
         return cur;
     }
 
-    public static void fillParentMap(Node head, HashMap<Node, Node> parentMap) {
+    public static void fillParentMap(TreeNode head, HashMap<TreeNode, TreeNode> parentMap) {
         if (head.left != null) {
             parentMap.put(head.left, head);
             fillParentMap(head.left, parentMap);
@@ -48,7 +48,8 @@ public class Code03_lowestAncestor {
         }
     }
 
-    public static Node lowestAncestor2(Node head, Node a, Node b) {
+    // 二叉树的递归套路解法
+    public static TreeNode lowestCommonAncestor(TreeNode head, TreeNode a, TreeNode b) {
         return process(head, a, b).ans;
     }
 
@@ -59,9 +60,9 @@ public class Code03_lowestAncestor {
         // 当前树有没有发现b节点
         public boolean findB;
         // 当前树找到的a和b的最低公共祖先是谁，如果没有在这棵树找到则设置为null
-        public Node ans;
+        public TreeNode ans;
 
-        public Info(boolean fA, boolean fB, Node an) {
+        public Info(boolean fA, boolean fB, TreeNode an) {
             findA = fA;
             findB = fB;
             ans = an;
@@ -69,7 +70,7 @@ public class Code03_lowestAncestor {
     }
 
     // 二叉树递归套路
-    public static Info process(Node x, Node a, Node b) {
+    public static Info process(TreeNode x, TreeNode a, TreeNode b) {
         // 1、处理空节点
         // 如果为空节点，则说明该节点没有a和b，也没有找到二者的最低公共祖先，返回相应的info
         if (x == null) {
@@ -87,15 +88,18 @@ public class Code03_lowestAncestor {
         // 同理
         boolean findB = (x == b) || leftInfo.findB || rightInfo.findB;
         // 先将ans设置为null
-        Node ans = null;
-        // 如果左子树已经找到答案了，则当前树的答案就是左子树的答案
+        TreeNode ans = null;
+        // 如果左子树已经找到答案了，则当前树的答案就是左子树的答案，此时a和b一定都在左子树
         if (leftInfo.ans != null) {
             ans = leftInfo.ans;
-            // 如果右子树已经找到答案了，则当前树的答案就是右子树的答案
+        // 如果右子树已经找到答案了，则当前树的答案就是右子树的答案，此时a和b一定都在左子树
         } else if (rightInfo.ans != null) {
             ans = rightInfo.ans;
-            // 除了上面两种情况，然后发现a和b节点都在x树上，那么说明a和b的最低公共祖先一定是x
+        // 除了上面两种情况，然后发现a和b节点都在x树上，那么说明a和b的最低公共祖先一定是x
         } else {
+            // 如果上面的leftInfo.ans和rightInfo.ans都是空，说明一定不存在a和b都在左子树或者都在右子树的情况
+            // 那么如果在X这棵树上还发现了a和b，那么只有一种可能，就是a和b分别在X的左右子树上
+            // 这种情况就说明a和b的最低公共祖先一定是x
             if (findA && findB) {
                 ans = x;
             }
@@ -106,34 +110,34 @@ public class Code03_lowestAncestor {
     }
 
     // for test
-    public static Node generateRandomBST(int maxLevel, int maxValue) {
+    public static TreeNode generateRandomBST(int maxLevel, int maxValue) {
         return generate(1, maxLevel, maxValue);
     }
 
     // for test
-    public static Node generate(int level, int maxLevel, int maxValue) {
+    public static TreeNode generate(int level, int maxLevel, int maxValue) {
         if (level > maxLevel || Math.random() < 0.5) {
             return null;
         }
-        Node head = new Node((int) (Math.random() * maxValue));
+        TreeNode head = new TreeNode((int) (Math.random() * maxValue));
         head.left = generate(level + 1, maxLevel, maxValue);
         head.right = generate(level + 1, maxLevel, maxValue);
         return head;
     }
 
     // for test
-    public static Node pickRandomOne(Node head) {
+    public static TreeNode pickRandomOne(TreeNode head) {
         if (head == null) {
             return null;
         }
-        ArrayList<Node> arr = new ArrayList<>();
+        ArrayList<TreeNode> arr = new ArrayList<>();
         fillPrelist(head, arr);
         int randomIndex = (int) (Math.random() * arr.size());
         return arr.get(randomIndex);
     }
 
     // for test
-    public static void fillPrelist(Node head, ArrayList<Node> arr) {
+    public static void fillPrelist(TreeNode head, ArrayList<TreeNode> arr) {
         if (head == null) {
             return;
         }
@@ -147,10 +151,10 @@ public class Code03_lowestAncestor {
         int maxValue = 100;
         int testTimes = 1000000;
         for (int i = 0; i < testTimes; i++) {
-            Node head = generateRandomBST(maxLevel, maxValue);
-            Node o1 = pickRandomOne(head);
-            Node o2 = pickRandomOne(head);
-            if (lowestAncestor1(head, o1, o2) != lowestAncestor2(head, o1, o2)) {
+            TreeNode head = generateRandomBST(maxLevel, maxValue);
+            TreeNode o1 = pickRandomOne(head);
+            TreeNode o2 = pickRandomOne(head);
+            if (lowestAncestor1(head, o1, o2) != lowestCommonAncestor(head, o1, o2)) {
                 System.out.println("Oops!");
             }
         }
