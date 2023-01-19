ff5b056e6d4f56631030fe779c9b07b6b81918e5
NULL2048 <973071263@qq.com>
2023-01-19 17:06:38 +0800

    生命游戏 生命游戏，是英国数学家约翰·何顿·康威在 1970 年发明的细胞自动机。 给定一个包含 m × n 个格子的面板，每一个格子都可以看成是一个细胞。 每个细胞都具有一个初始状态：1 即为活细胞（live），或 0 即为死细胞（dead）。每个细胞与其八个相邻位置（水平，垂直，对角线）的细胞都遵循以下四条生存定律： 	1. 如果活细胞周围八个位置的活细胞数少于两个，则该位置活细胞死亡； 	2. 如果活细胞周围八个位置有两个或三个活细胞，则该位置活细胞仍然存活； 	3. 如果活细胞周围八个位置有超过三个活细胞，则该位置活细胞死亡； 	4. 如果死细胞周围正好有三个活细胞，则该位置死细胞复活； 下一个状态是通过将上述规则同时应用于当前状态下的每个细胞所形成的，其中细胞的出生和死亡是同时发生的。给你 m x n 网格面板 board 的当前状态，返回下一个状态。 【数组】【矩阵】【位运算】【模拟】
    测试链接：289. 生命游戏 - 力扣（LeetCode）

---
+++ src/大厂刷题班/class34/Problem_0289_GameOfLife.java
@@ -0,0 +1,56 @@
+package 大厂刷题班.class34;
+
+// 有关这个游戏更有意思、更完整的内容：
+// https://www.bilibili.com/video/BV1rJ411n7ri
+// 也推荐这个up主
+// 数组  矩阵  位运算
+// https://leetcode.cn/problems/game-of-life/
+public class Problem_0289_GameOfLife {
+    public void gameOfLife(int[][] board) {
+        int m = board[0].length;
+        int n = board.length;
+        // 遍历所有位置，根据每一个位置周围1的数量来判断这个细胞是活还是死
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int curOneCnt = getOneCnt(board, i, j);
+                // 利用位信息，就可以不适用辅助数组了，同事保留旧信息和新信息
+                // 如果周围有3个1，那么这个细胞下一步已经活
+                if (curOneCnt == 3) {
+                    // 用末位的前一位来标识下一步的状态信息，所以和2取或运算
+                    board[i][j] |= 2;
+                    // 如果当前位置细胞是活的，并且它的周围有2个活细胞，那么下一步他也是活的
+                } else if (curOneCnt == 2 && board[i][j] == 1) {
+                    board[i][j] |= 2;
+                }
+
+                // 除了上面的情况以外，其他全部都是死
+            }
+        }
+
+        // 将每一个位置的信息右移一位，将旧信息抹去，只保留新信息
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] >>= 1;
+            }
+        }
+    }
+
+    // 统计(i,j)位置周围有多少个1
+    public int getOneCnt(int[][] board, int i, int j) {
+        return check(board, i - 1, j - 1) +
+                check(board, i - 1, j) +
+                check(board, i - 1, j + 1) +
+                check(board, i, j - 1) +
+                check(board, i, j + 1) +
+                check(board, i + 1, j - 1) +
+                check(board, i + 1, j) +
+                check(board, i + 1, j + 1);
+    }
+
+    public int check(int[][] board, int i, int j) {
+        int m = board[0].length;
+        int n = board.length;
+        // 如果当前位置不越界，并且这个位置的末位信息是1，那么就说明这个位置的细胞是活的，返回1，否则返回0
+        return i >= 0 && i < n && j >= 0 && j < m && (board[i][j] & 1) == 1 ? 1 : 0;
+    }
+}
