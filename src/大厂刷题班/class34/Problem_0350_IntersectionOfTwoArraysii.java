0d9b8271d0acfafe26ed6e828a7866086aba0654
NULL2048 <973071263@qq.com>
2023-01-31 09:50:26 +0800

    两个数组的交集Ⅱ 给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。 【数组】
    测试链接：350. 两个数组的交集 II - 力扣（LeetCode）

---
+++ src/大厂刷题班/class34/Problem_0350_IntersectionOfTwoArraysii.java
@@ -0,0 +1,29 @@
+package 大厂刷题班.class34;
+
+import java.util.Arrays;
+
+// 数组
+// https://leetcode.cn/problems/intersection-of-two-arrays-ii/
+public class Problem_0350_IntersectionOfTwoArraysii {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        // 词频统计表
+        int[] map = new int[1001];
+        // 记录要返回的答案
+        int[] ans = new int[1000];
+        int index = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            map[nums1[i]]++;
+        }
+
+        for (int i = 0; i < nums2.length; i++) {
+            map[nums2[i]]--;
+            // 这样就可以实现如果交集的个数不一样，取较小的那一个词频
+            if (map[nums2[i]] >= 0) {
+                ans[index++] = nums2[i];
+            }
+        }
+        // 阶段数组
+        return Arrays.copyOf(ans, index);
+    }
+}
