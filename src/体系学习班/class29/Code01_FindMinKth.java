c05992902b914aa961bbaabf01dd9f2f1e3e0c5e
NULL2048 <973071263@qq.com>
2023-03-08 22:56:29 +0800

    摆动排序Ⅱ 给你一个整数数组 nums，将它重新排列成 nums[0] < nums[1] > nums[2] < nums[3]... 的顺序。 你可以假设所有输入数组都可以得到满足题目要求的结果。
    示例 1：
    输入：nums = [1,5,1,1,6,4]
    输出：[1,6,1,5,1,4]
    解释：[1,4,1,5,1,6] 同样是符合题目要求的结果，可以被判题程序接受。
    
    示例 2：
    输入：nums = [1,3,2,2,3,1]
    输出：[2,3,1,3,1,2]
    
    进阶：你能用 O(n) 时间复杂度和原地 O(1) 额外空间来实现吗？
    【快速排序】【分治】【数组】【下标循环怼】
    
    增加自己编写的次优解代码，快速选择算法 + 三向切分算法（3-way-partition）

--- src/体系学习班/class29/Code01_FindMinKth.java
+++ src/体系学习班/class29/Code01_FindMinKth.java
@@ -151,6 +151,113 @@ public class Code01_FindMinKth {
         }
     }
 
+    // 4、自己写的次优解，如果最优解实在写不出来，就会写这个代码就够了
+    // 下面的代码可以直接提交力扣通过，这个代码更好理解一些，也更容易实现
+    // 快速选择算法 + 三向切分算法（3-way-partition）
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int mid = 0;
+        // 区分数组长度为奇数和偶数的情况
+        // 如果是奇数情况，数组划分的前半段要比后半段多1，并且mid也作为数组中间位置的下标
+        if ((n & 1) == 1) {
+            mid = n >> 1;
+        } else {
+            mid = (n >> 1) - 1;
+        }
+        // 设置数组a和数组b的长度，用来存储划分出来的两部分数据
+        // 如果nums长度为奇数，则an比bn多1
+        int an = mid + 1;
+        int bn = n - (mid + 1);
+        int[] a = new int[an];
+        int[] b = new int[bn];
+
+        // 使用快速选择法定位到数组的中位数，并且再用三向切分算法（3-way-partition）将小于中位数的数放到中位数左边，大于中位数的放到中位数右边
+        int l = 0;
+        int r = n - 1;
+        // 基准数字
+        int pivot = 0;
+        // 每一轮返回的三段分割点下标
+        int[] range = null;
+        // 快速选择法找中位数
+        while (l < r) {
+            // 这里用随机数随机生成一个位置的数做基准即可，并不会影响通过力扣测试，随机数不会造成错误的。这里也可以选择l~r范围内一个固定位置的数，例如(l + r) / 2位置的数作为基准
+            pivot = nums[l + (int) (Math.random() * (r - l + 1))];
+            // 三向切分算法来将数组划分成三部分
+            range = patition(nums, pivot, l, r);
+
+            // 判断我们是不是找到了中位数，如果mid落在了划分出来的中间相等部分，说明我们找到了中位数，就是此时的mid位置的数，大小是pivot
+            if (mid > range[0] && mid < range[1]) {
+                break;
+                // 只向一个方向递归，所以时间复杂度是O(N)
+            } else if (mid <= range[0]) {
+                r = range[0];
+            } else if (mid >= range[1]) {
+                l = range[1];
+            }
+        }
+
+        // 还是和方法1一样将两部分逆序，来避免相等的数相邻的情况
+        reverse(nums, 0, mid);
+        reverse(nums, mid + 1, n - 1);
+
+        // 将划分出来的两部分分别加入到a数组和b数组中
+        int index = 0;
+        for (int i = 0; i < an; i++) {
+            a[i] = nums[index++];
+        }
+        for (int i = 0; i < bn; i++) {
+            b[i] = nums[index++];
+        }
+
+        // 将a数组和b数组中的数据依次交叉覆写到nums中，完成摆动排序
+        index = 0;
+        int i = 0;
+        while (index < n) {
+            if (i < an) {
+                nums[index++] = a[i];
+            }
+            // 要记得判断i不能越界，因为在奇数长度情况下，bn<an
+            if (i < bn) {
+                nums[index++] = b[i++];
+            }
+        }
+    }
+
+    // 将小于pivot的数放到pivot左边，大于pivot的数放到pivot右边
+    public int[] patition(int[] nums, int pivot, int l, int r) {
+        // less左边是小于pivot的数，包括less位置
+        int less = l - 1;
+        // more右边是大于pivot的数，包括more位置
+        int more = r + 1;
+        // 当前遍历到的数
+        int cur = l;
+        // 这里要注意，条件是cur < more，不是cur < r
+        while (cur < more) {
+            // 遍历过程中如果当前位置的数小于pivot，则将less++，为小于区扩充一个位置，将这个数移动到扩充出来的这个位置，并且cur++去遍历下一个位置，因为交换到cur位置的数一定是等于pivot的
+            if (nums[cur] < pivot) {
+                swap(nums, ++less, cur++);
+                // 这个分支要注意
+                // 遍历过程中如果当前位置的数大于pivot，则将more++，为大于区扩充一个位置，将这个数移动到扩充出来的这个位置，但是cur不右移，因为交换到cur位置的数是我们还没有遍历到的，并不能确定他是不是等于pivot，所以在下一轮还需要继续判断它
+            } else if (nums[cur] > pivot) {
+                swap(nums, cur, --more);
+                // 相等，则cur右移
+            } else {
+                cur++;
+            }
+        }
+
+        // 结束之后就将数组划分成了以pivot为划分的三部分，返回划分断点位置
+        return new int[] {less, more};
+    }
+
+    // 逆序数组
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+
+
 
     // for test
     public static int[] generateRandomArray(int maxSize, int maxValue) {
