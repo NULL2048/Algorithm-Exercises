a8be71740c113ac1261d1a7fd7686ba9636894f5
NULL2048 <973071263@qq.com>
2023-03-08 10:02:05 +0800

    完善代码注释

--- src/体系学习班/class46/Code04_MaxSumLengthNoMore.java
+++ src/体系学习班/class46/Code04_MaxSumLengthNoMore.java
@@ -1,7 +1,6 @@
 package 体系学习班.class46;
 
 import java.util.LinkedList;
-
 // 给定一个数组arr，和一个正数M
 // 返回在子数组长度不大于M的情况下，最大的子数组累加和
 public class Code04_MaxSumLengthNoMore {
@@ -24,43 +23,59 @@ public class Code04_MaxSumLengthNoMore {
         }
         return max;
     }
+
     // O(N)的解法，最优解
-    // 用滑动窗口，尝试以每一个位置的数位窗口左边界看，求符合要求的最大子数组累加和，就让窗口不断地右移即可。
+    // 用窗口内最大值更新结构，尝试以每一个位置的数为窗口左边界，求符合要求的最大子数组累加和，就让窗口不断地右移更新最大值即可。
     public static int maxSum(int[] arr, int M) {
         if (arr == null || arr.length == 0 || M < 1) {
             return 0;
         }
         int N = arr.length;
         int[] sum = new int[N];
+        // 生成前缀和数组，用来快速求任意范围的子数组累加和
         sum[0] = arr[0];
         for (int i = 1; i < N; i++) {
             sum[i] = sum[i - 1] + arr[i];
         }
+        // 窗口内子数组累加和最大值更新结构的双端队列   这个里面存储的是累加和最大值子数组的结尾位置下标
         LinkedList<Integer> qmax = new LinkedList<>();
         int i = 0;
+        // 首先初始化窗口，取N和M的最小值来形成窗口以及累加和最大值更新结构
         int end = Math.min(N, M);
         for (; i < end; i++) {
+            // 更新流程和经典的结构完全一样，只不过这里比较的是累加和大小
             while (!qmax.isEmpty() && sum[qmax.peekLast()] <= sum[i]) {
                 qmax.pollLast();
             }
             qmax.add(i);
         }
+        // 记录当前长度符合条件的最大累加和
         int max = sum[qmax.peekFirst()];
+        // 尝试的窗口左边界   这里回尝试以每一个位置为开始位置的长度不超过M的子数组累加和是多少
         int L = 0;
+        // 开始窗口内累加和最大值更新结构的维护
+        // 因为每一次左边界都会右移一个位置，而且在前面已经固定好窗口长度不超过M了，所以在后续的更新过程中一定能保证子数组长度不超过M
         for (; i < N; L++, i++) {
+            // 弹出左边界的数   此时最大累加和的下标已经等于L了，说明需要尝试新的左边界了，弹出此时的累加和最大值下标L
             if (qmax.peekFirst() == L) {
                 qmax.pollFirst();
             }
+            // 更新累加和最大值
             while (!qmax.isEmpty() && sum[qmax.peekLast()] <= sum[i]) {
                 qmax.pollLast();
             }
             qmax.add(i);
+            // 更新max，注意因为弹出了左边界的数，所以这里要利用前缀和定律减去sum[L]，才能求出来以此时新的窗口左边界为开始位置的最大累加和
             max = Math.max(max, sum[qmax.peekFirst()] - sum[L]);
         }
+
+        // 有可能上面循环结束后，L还没有尝试完所有的位置，所以再继续尝试
+        // 后续的子数组长度一定是不超过M的，因为只会缩左边界了，不会扩右边界了
         for (; L < N - 1; L++) {
             if (qmax.peekFirst() == L) {
                 qmax.pollFirst();
             }
+            // 尝试以每个L开头的子数组长度不超过M情况下的最大累加和
             max = Math.max(max, sum[qmax.peekFirst()] - sum[L]);
         }
         return max;
--- src/测试/Solution.java
+++ src/测试/Solution.java
@@ -3,31 +3,51 @@ package 测试;
 import java.util.*;
 
 class Solution {
-    public static int lengthOfLongestSubstring(String str) {
-        if (str.length() <= 1) {
-            return str.length();
-        }
+    public static int[] getMaxMatrix(int[][] matrix) {
+        int max = Integer.MIN_VALUE;
+        int[] ans  = new int[4];
 
-        char[] s = str.toCharArray();
-        int l = 0;
-        int r = 0;
-        int[] count = new int[256];
-        int max = 0;
-        while (r < s.length) {
-            if (count[r] == 0) {
-                count[r++]++;
+        for (int k = 0; k < matrix.length; k++) {
+            int[] arr = new int[matrix[0].length];
 
-                max = Math.max(max, r - l);
-            } else {
-                count[l--]--;
+            for (int i = k; i < matrix.length; i++) {
+                int[] dp = new int[matrix[0].length];
+                int start = 0;
+                arr[0] += matrix[i][0];
+                dp[0] = arr[0];
+
+                if (dp[0] > max) {
+                    max = dp[0];
+                    ans[0] = k;
+                    ans[1] = 0;
+                    ans[2] = i;
+                    ans[3] = 0;
+                }
+
+                for (int j = 1; j < matrix[0].length; j++) {
+                    arr[j] += matrix[i][j];
+
+                    dp[j] = dp[j - 1] > 0 ? dp[j - 1] + arr[j] : arr[j];
+                    if (dp[j - 1] <= 0) {
+                        start = j;
+                    }
+
+                    if (dp[0] > max) {
+                        max = dp[0];
+                        ans[0] = k;
+                        ans[1] = start;
+                        ans[2] = i;
+                        ans[3] = j;
+                    }
+                }
             }
         }
 
-        return max;
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[][] grid = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        int[][] grid = {{9,-8,1,3,-2},{-3,7,6,-2,4},{6,-4,-4,8,-7}};
 
         int[] nums = {7,13,20,19,19,2,10,1,1,19};
         int[] nums2 = {3,4,5,1,2};
@@ -40,7 +60,11 @@ class Solution {
         String str1 = "abcabcbb";
         String str2 = "ABC";
 
-        System.out.println(lengthOfLongestSubstring(str1));
+        int[] ans = getMaxMatrix(grid);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + ' ');
+        }
+        //System.out.println(getMaxMatrix(grid));
 
 
 //        String ans = minWindow(nums, n);
