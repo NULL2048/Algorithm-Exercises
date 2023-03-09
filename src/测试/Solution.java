618804040bfd4a6763689075198718c567ebff6a
NULL2048 <973071263@qq.com>
2023-03-09 17:37:06 +0800

    完善代码注释

--- src/测试/Solution.java
+++ src/测试/Solution.java
@@ -3,54 +3,65 @@ package 测试;
 import java.util.*;
 
 class Solution {
-    public static int[] getMaxMatrix(int[][] matrix) {
-        int max = Integer.MIN_VALUE;
-        int[] ans  = new int[4];
+    public static void wiggleSort(int[] nums) {
+        Arrays.sort(nums);
 
-        for (int k = 0; k < matrix.length; k++) {
-            int[] arr = new int[matrix[0].length];
-
-            for (int i = k; i < matrix.length; i++) {
-                int[] dp = new int[matrix[0].length];
-                int start = 0;
-                arr[0] += matrix[i][0];
-                dp[0] = arr[0];
-
-                if (dp[0] > max) {
-                    max = dp[0];
-                    ans[0] = k;
-                    ans[1] = 0;
-                    ans[2] = i;
-                    ans[3] = 0;
-                }
-
-                for (int j = 1; j < matrix[0].length; j++) {
-                    arr[j] += matrix[i][j];
-
-                    dp[j] = dp[j - 1] > 0 ? dp[j - 1] + arr[j] : arr[j];
-                    if (dp[j - 1] <= 0) {
-                        start = j;
-                    }
-
-                    if (dp[0] > max) {
-                        max = dp[0];
-                        ans[0] = k;
-                        ans[1] = start;
-                        ans[2] = i;
-                        ans[3] = j;
-                    }
-                }
-            }
+        int mid = nums.length >> 1;
+        if ((nums.length & 1) == 1) {
+            reverse(nums, 0, mid);
+            reverse(nums, mid + 1, nums.length - 1);
+        } else {
+            reverse(nums, 0, mid - 1);
+            reverse(nums, mid, nums.length - 1);
         }
 
-        return ans;
+
+        int[] a = null;
+        if ((nums.length & 1) == 1) {
+            a = new int[mid + 1];
+        } else {
+            a = new int[mid];
+        }
+        int[] b = new int[mid];
+
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = nums[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            b[i] = nums[i + mid];
+        }
+
+        int index = 0;
+        int i = 0;
+        while (index < nums.length) {
+            if (i < a.length) {
+                nums[index++] = a[i];
+            }
+
+            if (i < b.length) {
+                nums[index++] = b[i++];
+            }
+        }
+    }
+
+    public static void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            swap(nums, l++, r--);
+        }
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
     public static void main(String[] args) {
         int[][] grid = {{9,-8,1,3,-2},{-3,7,6,-2,4},{6,-4,-4,8,-7}};
 
         int[] nums = {7,13,20,19,19,2,10,1,1,19};
-        int[] nums2 = {3,4,5,1,2};
+        int[] nums2 = {1,1,2,1,2,2,1};
         int n = 3;
 
         char[] task = {'A','A','A','B','B','B'};
@@ -60,9 +71,9 @@ class Solution {
         String str1 = "abcabcbb";
         String str2 = "ABC";
 
-        int[] ans = getMaxMatrix(grid);
-        for (int i = 0; i < ans.length; i++) {
-            System.out.print(ans[i] + ' ');
+        wiggleSort(nums2);
+        for (int i = 0; i < nums2.length; i++) {
+            System.out.print(nums2[i] + ' ');
         }
         //System.out.println(getMaxMatrix(grid));
 
