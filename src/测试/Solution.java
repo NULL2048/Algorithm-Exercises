b6e87ffcc4ea2529b3f4ef64098e9447247201ad
NULL2048 <973071263@qq.com>
2023-03-23 22:22:10 +0800

    完善代码注释

--- src/测试/Solution.java
+++ src/测试/Solution.java
@@ -3,62 +3,60 @@ package 测试;
 import java.util.*;
 
 class Solution {
-    public static void wiggleSort(int[] nums) {
-        Arrays.sort(nums);
-
-        int mid = nums.length >> 1;
-        if ((nums.length & 1) == 1) {
-            reverse(nums, 0, mid);
-            reverse(nums, mid + 1, nums.length - 1);
-        } else {
-            reverse(nums, 0, mid - 1);
-            reverse(nums, mid, nums.length - 1);
+    public static int numBusesToDestination(int[][] routes, int source, int target) {
+        if (source == target) {
+            return 0;
         }
 
-
-        int[] a = null;
-        if ((nums.length & 1) == 1) {
-            a = new int[mid + 1];
-        } else {
-            a = new int[mid];
-        }
-        int[] b = new int[mid];
-
-
-        for (int i = 0; i < a.length; i++) {
-            a[i] = nums[i];
-        }
-        for (int i = 0; i < b.length; i++) {
-            b[i] = nums[i + mid];
-        }
-
-        int index = 0;
-        int i = 0;
-        while (index < nums.length) {
-            if (i < a.length) {
-                nums[index++] = a[i];
-            }
-
-            if (i < b.length) {
-                nums[index++] = b[i++];
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        // 构造每一个车站能够直接到达的公交线路
+        for (int i = 0; i < routes.length; i++) {
+            for (int j = 0; j < routes[i].length; j++) {
+                if (!map.containsKey(routes[i][j])) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    map.put(routes[i][j], list);
+                } else {
+                    map.get(routes[i][j]).add(i);
+                }
             }
         }
-    }
 
-    public static void reverse(int[] nums, int l, int r) {
-        while (l < r) {
-            swap(nums, l++, r--);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        boolean[] set = new boolean[routes.length];
+        for (Integer line : map.get(source)) {
+            queue.add(line);
+            set[line] = true;
         }
-    }
+        int cnt = 0;
 
-    public static void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+        while (!queue.isEmpty()) {
+            Queue<Integer> next = new LinkedList<Integer>();
+
+            for (Integer line : queue) {
+                //if (!set[line]) {
+                for (Integer station : routes[line]) {
+                    if (station == target) {
+                        return cnt + 1;
+                    }
+                    for (Integer newLine : map.get(station)) {
+                        if (!set[line]) {
+                            next.add(newLine);
+                            set[newLine] = true;
+                        }
+                    }
+                }
+                // }
+            }
+
+            queue = next;
+            cnt++;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        int[][] grid = {{9,-8,1,3,-2},{-3,7,6,-2,4},{6,-4,-4,8,-7}};
+        int[][] grid = {{1,2,7},{3,6,7}};
 
         int[] nums = {7,13,20,19,19,2,10,1,1,19};
         int[] nums2 = {1,1,2,1,2,2,1};
@@ -71,10 +69,10 @@ class Solution {
         String str1 = "abcabcbb";
         String str2 = "ABC";
 
-        wiggleSort(nums2);
-        for (int i = 0; i < nums2.length; i++) {
-            System.out.print(nums2[i] + ' ');
-        }
+        System.out.println(numBusesToDestination(grid, 1, 6));
+//        for (int i = 0; i < nums2.length; i++) {
+//            System.out.print(nums2[i] + ' ');
+//        }
         //System.out.println(getMaxMatrix(grid));
 
 
