ca3b6054d61e06dad7a13885add924706ef789cf
NULL2048 <973071263@qq.com>
2023-06-29 16:58:51 +0800

    完善代码注释

--- src/测试/Solution.java
+++ src/测试/Solution.java
@@ -3,59 +3,53 @@ package 测试;
 import java.util.*;
 
 class Solution {
-        public static int lengthOfLIS(int[] nums) {
-            int[] dp = new int[nums.length];
-            dp[0] = 1;
+    public static int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] lens = new int[n];
+        int[] cnts = new int[n];
+        lens[0] = 1;
+        cnts[0] = 1;
+        int allCnt = 1;
+        int maxLen = 1;
 
-            for (int i = 1; i < nums.length; i++) {
-                int max = -1;
-                for (int j = 0; j < i; j++) {
-                    if (nums[i] > nums[j]) {
-                        max = Math.max(max, dp[j]);
-                    }
+        for (int i = 1; i < n; i++) {
+            int preLen = 0;
+            //
+            int preCnt = 0;
+
+            for (int j = 0; j < i; j++) {
+                //
+                if (nums[j] > nums[i] || lens[j] < preLen) {
+                    continue;
                 }
 
-                dp[i] = max != -1 ? max + 1 : 1;
+                if (lens[j] > preLen) {
+                    preLen = lens[j];
+                    preCnt = cnts[j];
+                } else {
+                    preCnt += cnts[j];
+                }
             }
 
+            lens[i] = preLen + 1;
+            cnts[i] = preCnt;
 
-            return dp[nums.length - 1];
+            if (lens[i] > maxLen) {
+                maxLen = lens[i];
+                allCnt = cnts[i];
+            } else if (lens[i] == maxLen) {
+                allCnt += cnts[i];
+            }
         }
 
-
-        // public int process(int[] nums, int index) {
-        //     if (index == 0) {
-        //         return 1;
-        //     }
-
-        //     int p1 =
-        // }
-
-        // public int process(int[] nums, int index, int pre, HashMap<Integer, HashMap<Integer, Integer>> dp) {
-        //     if (index == nums.length) {
-        //         return 0;
-        //     }
-
-        //     if (dp.containsKey(index) && dp.get(index).containsKey(pre)) {
-        //         return dp.get(index).get(pre);
-        //     }
-
-        //     int p1 = pre < nums[index] ? process(nums, index + 1, nums[index], dp) + 1 : 0;
-        //     int p2 = process(nums, index + 1, pre, dp);
-
-        //     if (!dp.containsKey(index)) {
-        //         dp.put(index, new HashMap<Integer, Integer>());
-        //     }
-
-        //     dp.get(index).put(pre, Math.max(p1, p2));
-        //     return Math.max(p1, p2);
-        // }
+        return allCnt;
+    }
 
     public static void main(String[] args) {
         int[][] grid = {{1,2,7},{3,6,7}};
 
         int[] nums = {2,3,6,7};
-        int[] nums2 = {1,3,6,7,9,4,10,5,6};
+        int[] nums2 = {2,2,2,2,2};
         int n = 3;
 
         char[] task = {'A','A','A','B','B','B'};
@@ -65,7 +59,7 @@ class Solution {
         String str1 = "abcabcbb";
         String str2 = "ABC";
 
-        System.out.println(lengthOfLIS(nums2));
+        System.out.println(findNumberOfLIS(nums2));
 
 //        for (int i = 0; i < nums2.length; i++) {
 //            System.out.print(nums2[i] + ' ');
