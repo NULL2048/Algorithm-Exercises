6c1eb34d2d4e0b43ed835fd9f5eb3ec8687173bd
NULL2048 <973071263@qq.com>
2023-07-19 13:55:44 +0800

    完善代码

--- src/大厂刷题班/class15/Code03_BestTimeToBuyAndSellStockIV.java
+++ src/大厂刷题班/class15/Code03_BestTimeToBuyAndSellStockIV.java
@@ -3,6 +3,36 @@ package 大厂刷题班.class15;
 // 测试链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv/
 // 从左往右的尝试模型
 public class Code03_BestTimeToBuyAndSellStockIV {
+    // 1、动态规划代码
+    public int maxProfit1(int k, int[] prices) {
+        // dp[i][j]：只能在arr 0...i上做交易，而且交易次数不要超过j次获得的最大收益
+        // dp[i][0]和dp[0][j]都是0
+        int[][] dp = new int[prices.length][k + 1];
+
+        // 开始给dp赋值  从i=1开始，因为i=0的dp都为0
+        for (int i = 1; i < prices.length; i++) {
+            // 从j=1开始，因为j=0的dp都为0
+            for (int j = 1; j <= k; j++) {
+                // 情况1：在prices[i]的价格时不去做交易，所以这种情况下的dp[i][j] = dp[i - 1][j]
+                int p1 = dp[i - 1][j];
+                // 情况2：在prices[i]的价格时去做交易，我们要以i时间为最后一次交易的卖出时间，也就是i时间卖出prices[i]元
+                // 下面我们就需要去找所有买入时间点的情况，从所有情况中找一个盈利最大值
+                int p2 = 0;
+                // p就是尝试第j次交易的所有买入时间点
+                for (int p = 0; p <= i; p++) {
+                    // dp[p][j - 1](在p之前已经完成了j-1次交易的最大盈利) - prices[p](在p时间以prices[p]价格买入) + prices[i](在i时间以prices[i]价格卖出)
+                    // 取所有可能情况的总盈利最大值
+                    p2 = Math.max(p2, dp[p][j - 1] - prices[p] + prices[i]);
+                }
+                // 取情况1和情况2之间的最大值
+                dp[i][j] = Math.max(p1, p2);
+            }
+        }
+        // 0~prices.length - 1时间交易k次的最大收益
+        return dp[prices.length - 1][k];
+    }
+
+    // 2、斜率优化代码
     public int maxProfit(int k, int[] prices) {
         // 过滤无效参数
         if (prices == null || prices.length == 0 || k == 0) {
